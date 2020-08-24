7a767c36a25394c307114a130a32744b3d8bc8e1
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-08-24 21:18:07 +0900

    fix: iterm2

--- iterm2/init.sh
+++ iterm2/init.sh
@@ -2,9 +2,6 @@
 
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 
-mkdir -p ~/iterm2
-cp $CURRENT_DIR/Iceberg-iTerm2/iceberg.itermcolors ~/iterm2/iceberg.itermcolors
-
 ln -fs $CURRENT_DIR/com.googlecode.iterm2.plist ~/Library/Preferences
 
 # 設定の再読み込み
