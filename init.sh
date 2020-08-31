b143bba6ee3430cdfffc9b6174bfe2b1f969a52c
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-08-31 09:49:06 +0900

    fix: init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-CURRENT_DIR=$(cd $(dirname $0); pwd)
+set -ue
+
+CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
 
 for f in \
 	".bash_profile" \
 	".bashrc" \
 	".gitconfig"; do
-  echo "symbolic link: $f"
-	ln -fs $CURRENT_DIR/$f ~
+  echo "symbolic link: $CURRENT_DIR/$f"
+	ln -fs "$CURRENT_DIR/$f" ~
 done
