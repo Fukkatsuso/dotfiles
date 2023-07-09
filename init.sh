6ff1a82319cc9fd597fb01e461663620449456ed
Fukkatsuso <tatsuki.mutsuro@dena.com>
2023-07-09 17:06:12 +0900

    delete: unnecessary gitconfig

--- init.sh
+++ init.sh
@@ -7,8 +7,7 @@ CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
 for f in \
 	".bash_profile" \
 	".bashrc" \
-	".gitconfig" \
-	".gitconfig-aiot"; do
+	".gitconfig"; do
   echo "symbolic link: $CURRENT_DIR/$f"
 	ln -fs "$CURRENT_DIR/$f" ~
 done
